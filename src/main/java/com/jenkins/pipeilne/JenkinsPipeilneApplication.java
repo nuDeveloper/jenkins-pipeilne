c1e50207dff45d2bfd31728b393111505044e2aa
nuDeveloper <anandkumarsharma005@gmail.com>
2025-04-12 14:02:23 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/pipeilne/JenkinsPipeilneApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.pipeilne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipeilneApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipeilneApplication.class, args);
+		System.out.println("Jenkins CI/CD demo...");
+    }
+}
---
+++ src/test/java/com/jenkins/pipeilne/JenkinsPipeilneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.pipeilne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipeilneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
