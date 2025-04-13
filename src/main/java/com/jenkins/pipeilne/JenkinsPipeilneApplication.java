41f6ab71264a50186708b76e38ccf873fa5655e3
nuDeveloper <anandkumarsharma005@gmail.com>
2025-04-13 14:40:57 +0530

    changes to trigger jenkins build

--- src/main/java/com/jenkins/pipeilne/JenkinsPipeilneApplication.java
+++ src/main/java/com/jenkins/pipeilne/JenkinsPipeilneApplication.java
@@ -9,5 +9,7 @@ public class JenkinsPipeilneApplication {
     public static void main(String[] args) {
 		SpringApplication.run(JenkinsPipeilneApplication.class, args);
 		System.out.println("Jenkins CI/CD demo...");
+
+        System.out.println("Some changes to trigger jenkins build");
     }
 }
